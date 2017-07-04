f51b1b5e1c7a805bfe5c3fa7c400dbddd2320712
sgrinev <sgrinev@qua2>
2017-07-04 18:10:49 +0300

    more chapter 1 one code snippets

--- Chapter1/src/demo/FXModality.java
+++ Chapter1/src/demo/FXModality.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fxparams;
+package demo;
 
 import javafx.application.Application;
 import javafx.scene.*;
---
+++ Chapter1/src/demo/HelloFX.java
@@ -0,0 +1,50 @@
+package demo;
+
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sgrinev
+ */
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+
+        CheckBox node = new CheckBox("I'm ready for FX!");
+        Rectangle rect = new Rectangle(140, 70, Color.LIGHTGRAY);
+        root.getChildren().addAll(rect, node);
+
+        Scene scene = new Scene(root, 150, 100);
+
+        stage.setScene(scene);
+        stage.setTitle("Hello FX!");
+        stage.show();
+
+        traverse(root, 0);
+    }
+
+    public void traverse(Node node, int level) {
+        for (int i = 0; i < level; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(node.getClass());
+        if (node instanceof Parent) {
+            Parent parent = (Parent) node;
+            parent.getChildrenUnmodifiable().forEach(n->traverse(n, level +1));
+        }
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
