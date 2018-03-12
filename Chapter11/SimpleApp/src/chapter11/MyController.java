5e42e0702c004f78001ccd10b4f43965d555fb37
sgrinev <sergeygrinev@mail.ru>
2018-03-12 11:52:09 +0300

    chapter11 demo project

---
+++ Chapter11/SimpleApp/src/chapter11/MyController.java
@@ -0,0 +1,32 @@
+/*
+ * (C) Packt Publishing Ltd, 2017-2018
+ */
+package chapter11;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author sgrinev
+ */
+public class MyController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Second Stage is being loaded.");
+    }    
+    
+}
---
+++ Chapter11/SimpleApp/src/chapter11/SimpleApp.java
@@ -0,0 +1,58 @@
+/*
+ * (C) Packt Publishing Ltd, 2017-2018
+ */
+package chapter11;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author sgrinev
+ */
+public class SimpleApp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Open Second Window");
+        btn.setOnAction((ActionEvent event) -> {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("SecondStage.fxml"));
+                Stage secondStage = new Stage(StageStyle.UTILITY);
+                secondStage.setScene(new Scene(root));
+                secondStage.show();
+            } catch (IOException ex) {
+                System.out.println("Failed to load FXML");
+                ex.printStackTrace();
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 200, 200);
+        
+        primaryStage.setTitle("SimpleApp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
