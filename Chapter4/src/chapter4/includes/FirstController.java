d119bd479d2fb670bf417d6e09e2255f75710c19
Sergey Grinev <sergeygrinev@mail.ru>
2018-05-06 04:58:55 +0300

    minor fixes in c4

--- Chapter4/src/chapter4/includes/FirstController.java
+++ Chapter4/src/chapter4/includes/FirstController.java
@@ -8,7 +8,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 /**
